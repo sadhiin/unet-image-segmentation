7e90ba059fffc0df4f2f4de5b81384a2411a713e
sadhiin <sanjidulislamsadhin1994@gmail.com>
2024-12-02 12:55:04 +0000

    updated the command for locally downloaded data

--- run.sh
+++ run.sh
@@ -1,40 +1,48 @@
-## -------------- For Experiment with KITTI Dataset --------------
-# python train.py \
-#     --dataset kitti \
-#     --data_dir unet-image-segmentation/data \
-#     --image_size 256 \
-#     --batch_size 16 \
-#     --epochs 2 \
-#     --learning_rate 0.00001 \
-#     --num_workers 2 \
-#     --use_wandb
+# -------------- For Experiment with KITTI Dataset --------------
+## if the dataset is downloaded, then you can replace the data_dir with the path to the dataset as follows:
+# --data_dir data/kitti-dataset
+python train.py \
+    --dataset kitti \
+    --data_dir data \
+    --image_size 256 \
+    --batch_size 16 \
+    --epochs 2 \
+    --learning_rate 0.00001 \
+    --num_workers 2 \
+    --use_wandb
 
-## -------------- For Experiment with CityScapes Dataset --------------
-# python train.py \
-#     --dataset cityscapes \
-#     --data_dir unet-image-segmentation/data \
-#     --image_size 256 \
-#     --batch_size 16 \
-#     --epochs 2 \
-#     --learning_rate 0.00001 \
-#     --num_workers 2 \
-#     --use_wandb
+# -------------- For Experiment with CityScapes Dataset --------------
+## if the dataset is downloaded, then you can replace the data_dir with the path to the dataset as follows:
+# --data_dir data/cityscapes_data
+python train.py \
+    --dataset cityscapes \
+    --data_dir data \
+    --image_size 256 \
+    --batch_size 16 \
+    --epochs 2 \
+    --learning_rate 0.00001 \
+    --num_workers 2 \
+    --use_wandb
 
-## -------------- For Experiment with Pascal VOC Dataset --------------
-# python train.py \
-#     --dataset pascal_voc \
-#     --data_dir unet-image-segmentation/data \
-#     --image_size 256 \
-#     --batch_size 16 \
-#     --epochs 5 \
-#     --learning_rate 0.0001 \
-#     --num_workers 2 \
-#     --use_wandb
+# -------------- For Experiment with Pascal VOC Dataset --------------
+## if the dataset is downloaded, then you can replace the data_dir with the path to the dataset as follows:
+# --data_dir data/pascal_voc
+python train.py \
+    --dataset pascal_voc \
+    --data_dir data \
+    --image_size 256 \
+    --batch_size 16 \
+    --epochs 5 \
+    --learning_rate 0.0001 \
+    --num_workers 2 \
+    --use_wandb
 
 ## -------------- For Experiment with COCO Dataset --------------
+## if the dataset is downloaded, then you can replace the data_dir with the path to the dataset as follows:
+# --data_dir data/coco2017
 python train.py \
     --dataset coco \
-    --data_dir unet-image-segmentation/data \
+    --data_dir data \
     --image_size 256 \
     --batch_size 16 \
     --epochs 2 \
